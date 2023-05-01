0195a365a77788c298cdf53bc044dd540501ec31
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2023-05-01 20:23:56 +0530

    Changes

--- src/Screens/SplashScreen.js
+++ src/Screens/SplashScreen.js
@@ -1,13 +1,12 @@
-import React, { useEffect } from "react";
-import { Dimensions, StyleSheet, Text, View } from "react-native";
 import Lottie from "lottie-react-native";
-import noterr from "../../assets/noterr.json";
+import React, { useEffect } from "react";
+import { StyleSheet, View } from "react-native";
 
 const SplashScreen = ({ navigation }) => {
     useEffect(() => {
         setTimeout(() => {
             navigation.replace("Home");
-        }, 3000);
+        }, 2000);
     }, []);
     return (
         <View style={styles.container}>
